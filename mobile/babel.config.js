c1792f83bab5a2fba12ffe535860e182dda44fdd
Max Hopaytsa <gopayts2@gmail.com>
2023-10-09 18:27:07 +0300

    add mobile folder and install react-native

---
+++ mobile/babel.config.js
@@ -0,0 +1,21 @@
+const path = require('node:path');
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    [
+      'module-resolver',
+      {
+        root: path.resolve('./'),
+        alias: {
+          '^shared/build(.+)': path.resolve(
+            __dirname,
+            '../shared/build/cjs/\\1',
+          ),
+          '^#(.+)': './src/\\1',
+        },
+      },
+    ],
+  ],
+};
